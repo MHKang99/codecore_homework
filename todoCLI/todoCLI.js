8b812702942e0bd120fe4cc36f35462b09bc9975
Min <minkang99@hotmail.com>
2022-02-17 22:06:05 -0800

    made into functions instead of just if statments

--- todoCLI/todoCLI.js
+++ todoCLI/todoCLI.js
@@ -6,66 +6,143 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
-// must make with functions
+// --------------made with functions---------------
 
-let list = []
-let greeting = 0;
+let list = []; // storage for added list items 
+// removed the greeting value since using functions won't need it
 let toDoCLI = function () {
-    if (greeting === 0) {
-        console.log("\nWelcome to Todo CLI!\n\n-------------------\n")
-    }
-    rl.question("(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit\n", (answer) =>{
+    console.log("\nWelcome to Todo CLI!\n\n-------------------\n") // displays only when starting the file
+    view();
+
+    // view function that calls all the other functions depending on what the answer to the rl.question is
+    function view() { 
+        rl.question("(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit\n",(answer)=> {
+            if (answer === 'v') {
+                if (list.length === 0) {
+                    console.log("List is empty...");
         
-        if (answer === "v") {
-            if (list.length === 0) {
-                console.log("List is empty...")
-                greeting = 1;
-                toDoCLI();
-            } else {
-                console.log("\n")
-                for (let i = 0; i < list.length; i++) {
-                    console.log(i + " " + list[i])
+                    view(); 
+                }else {
+                    console.log("\n")
+                    for (let i = 0; i < list.length; i++) {
+                        console.log(i + " " + list[i])
+                    }
+                    console.log("\n")
+        
+                    view();
                 }
-                console.log("\n")
-                greeting = 1;
-                toDoCLI();
+            } else if (answer === 'n') {
+                newListItem();
+            } else if (answer[0] === 'c' && answer.length > 1) {
+                complete(answer);
+            } else if (answer[0] === 'd' && answer.length > 1) {
+                remove(answer);
+            } else if (answer === 'q') {
+                quit();
+            }else {
+                console.log("not a valid option")
+                view();
             }
-        } else if (answer === "n") {
-            rl.question("What?\n", (answer)=> {
-                list.push(`[ ] ${answer}`)
-                greeting = 1;
-                toDoCLI();
-            });
-            
-        } else if (answer[0] === "c") {
-            let indexNum = "";
-            for (let i = 1; i < answer.length; i++) {
-                indexNum += answer[i]
-            }
-            list.splice(parseInt(indexNum),1,list[parseInt(indexNum)].slice(0,1) + "✓" +list[parseInt(indexNum)].slice(2,list[parseInt(indexNum)].length));
-            console.log(`Completed "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`)
-            greeting = 1;
-            toDoCLI();
-        }else if (answer[0] === "d") {
-            let indexNum = "";
-            for (let i = 1; i < answer.length; i++) {
-                indexNum += answer[i]
-            }
-            console.log(`Deleted "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`);
-            list.splice(parseInt(indexNum),1);
-            greeting = 1;
-            toDoCLI();
-        } else if (answer === "q") {
-            console.log("See you soon!")
-            greeting = 0;
-            rl.close();
-        } else {
-            console.log("not a valid option")
-            greeting = 1;
-            toDoCLI();
+        })
+    }
+
+    function quit () {
+        console.log("See you soon!")
+        rl.close();
+    }
+
+    function remove (answer) {
+        let indexNum = "";
+        for (let i = 1; i < answer.length; i++) {
+            indexNum += answer[i]
         }
-    })
+        console.log(`Deleted "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`);
+        list.splice(parseInt(indexNum),1);
+        view();
+    }
+
+    function complete(answer) {
+        let indexNum = "";
+        for (let i = 1; i < answer.length; i++) {
+            indexNum += answer[i]
+        }
+        list.splice(parseInt(indexNum),1,list[parseInt(indexNum)].slice(0,1) + "✓" + list[parseInt(indexNum)].slice(2,list[parseInt(indexNum)].length));
+        console.log(`Completed "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`)
+        view();
+    }
+
+    function newListItem () {
+        rl.question("What?\n", (answer)=> {
+            list.push(`[ ] ${answer}`);
+            view();
+        })
+    }
 }
 
-
 toDoCLI()
+
+
+
+// --------- made with if statements but not functions -----------------
+
+// let list = []
+// let greeting = 0;
+// let toDoCLI = function () {
+//     if (greeting === 0) {
+//         console.log("\nWelcome to Todo CLI!\n\n-------------------\n")
+//     }
+//     rl.question("(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit\n", (answer) =>{
+        
+//         if (answer === "v") {
+//             if (list.length === 0) {
+//                 console.log("List is empty...")
+//                 greeting = 1;
+//                 toDoCLI();
+//             } else {
+//                 console.log("\n")
+//                 for (let i = 0; i < list.length; i++) {
+//                     console.log(i + " " + list[i])
+//                 }
+//                 console.log("\n")
+//                 greeting = 1;
+//                 toDoCLI();
+//             }
+//         } else if (answer === "n") {
+//             rl.question("What?\n", (answer)=> {
+//                 list.push(`[ ] ${answer}`)
+//                 greeting = 1;
+//                 toDoCLI();
+//             });
+            
+//         } else if (answer[0] === "c") {
+//             let indexNum = "";
+//             for (let i = 1; i < answer.length; i++) {
+//                 indexNum += answer[i]
+//             }
+//             list.splice(parseInt(indexNum),1,list[parseInt(indexNum)].slice(0,1) + "✓" +list[parseInt(indexNum)].slice(2,list[parseInt(indexNum)].length));
+//             console.log(`Completed "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`)
+//             greeting = 1;
+//             toDoCLI();
+//         }else if (answer[0] === "d") {
+//             let indexNum = "";
+//             for (let i = 1; i < answer.length; i++) {
+//                 indexNum += answer[i]
+//             }
+//             console.log(`Deleted "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`);
+//             list.splice(parseInt(indexNum),1);
+//             greeting = 1;
+//             toDoCLI();
+//         } else if (answer === "q") {
+//             console.log("See you soon!")
+//             greeting = 0;
+//             rl.close();
+//         } else {
+//             console.log("not a valid option")
+//             greeting = 1;
+//             toDoCLI();
+//         }
+//     })
+// }
+
+
+// toDoCLI()
--- todoCLI/workspace.js
+++ todoCLI/workspace.js
@@ -1,30 +1,147 @@
-// const fs = require("fs");
-// const readline = require("readline");
+const fs = require("fs");
+const readline = require("readline");
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// })
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-// rl.question("press cX where x is a number", (answer) => {
-//     console.log(typeof answer[1])
+// must make with functions
 
-//     // if (typeof answer[1] == "number") {
-//     //     console.log('good job it works')
-//     // } else console.log("doesnt work");
-// })
- 
-let list = [
-    "[✓] Take out the trash",
-    "[✓] Buy toothpaste",
-    "[ ] Buy Snickerdoodles",
-    "[ ] Fix the climate",
-    "[ ] Find a cure for aging",]
 
-console.log(list[3].slice(0,1) + "✓" +list[3].slice(2,list[3].length))
-let answer = "c90";
-let indexNum = "";
-for (let i = 1; i < answer.length; i++) {
-    indexNum += answer[i]
+
+let list = []
+let toDoCLI = function () {
+    console.log("\nWelcome to Todo CLI!\n\n-------------------\n")
+    view();
+    
+    function view() {
+        rl.question("(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit\n",(answer)=> {
+            if (answer === 'v') {
+                if (list.length === 0) {
+                    console.log("List is empty...");
+                    greeting = 1;
+                    view(); 
+                }else {
+                    console.log("\n")
+                    for (let i = 0; i < list.length; i++) {
+                        console.log(i + " " + list[i])
+                    }
+                    console.log("\n")
+                    greeting = 1;
+                    view();
+                }
+            } else if (answer === 'n') {
+                newListItem();
+            } else if (answer[0] === 'c' && answer.length > 1) {
+                complete(answer);
+            } else if (answer[0] === 'd' && answer.length > 1) {
+                remove(answer);
+            } else if (answer === 'q') {
+                quit();
+            }else {
+                console.log("not a valid option")
+                greeting = 1;
+                view();
+                        }
+        })
+    }
+
+    function quit () {
+        console.log("See you soon!")
+        greeting = 0;
+        rl.close();
+    }
+
+
+    function remove (answer) {
+        let indexNum = "";
+        for (let i = 1; i < answer.length; i++) {
+            indexNum += answer[i]
+        }
+        console.log(`Deleted "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`);
+        list.splice(parseInt(indexNum),1);
+        greeting = 1;
+        view();
+    }
+
+    function complete(answer) {
+        let indexNum = "";
+        for (let i = 1; i < answer.length; i++) {
+            indexNum += answer[i]
+        }
+        list.splice(parseInt(indexNum),1,list[parseInt(indexNum)].slice(0,1) + "✓" + list[parseInt(indexNum)].slice(2,list[parseInt(indexNum)].length));
+        console.log(`Completed "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`)
+        greeting = 1;
+        view();
+    }
+
+    function newListItem () {
+        rl.question("What?\n", (answer)=> {
+            list.push(`[ ] ${answer}`);
+            greeting = 1;
+            view();
+        })
+    }
 }
-console.log(typeof indexNum)
+
+// let greeting = 0;
+// let toDoCLI = function () {
+//     if (greeting === 0) {
+//         console.log("\nWelcome to Todo CLI!\n\n-------------------\n")
+//     }
+//     rl.question("(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit\n", (answer) =>{
+        
+//         if (answer === "v") {
+//             if (list.length === 0) {
+//                 console.log("List is empty...")
+//                 greeting = 1;
+//                 toDoCLI();
+//             } else {
+//                 console.log("\n")
+//                 for (let i = 0; i < list.length; i++) {
+//                     console.log(i + " " + list[i])
+//                 }
+//                 console.log("\n")
+//                 greeting = 1;
+//                 toDoCLI();
+//             }
+//         } else if (answer === "n") {
+//             rl.question("What?\n", (answer)=> {
+//                 list.push(`[ ] ${answer}`)
+//                 greeting = 1;
+//                 toDoCLI();
+//             });
+            
+//         } else if (answer[0] === "c") {
+//             let indexNum = "";
+//             for (let i = 1; i < answer.length; i++) {
+//                 indexNum += answer[i]
+//             }
+//             list.splice(parseInt(indexNum),1,list[parseInt(indexNum)].slice(0,1) + "✓" +list[parseInt(indexNum)].slice(2,list[parseInt(indexNum)].length));
+//             console.log(`Completed "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`)
+//             greeting = 1;
+//             toDoCLI();
+//         }else if (answer[0] === "d") {
+//             let indexNum = "";
+//             for (let i = 1; i < answer.length; i++) {
+//                 indexNum += answer[i]
+//             }
+//             console.log(`Deleted "${list[parseInt(indexNum)].slice(4,list[parseInt(indexNum)].length)}"`);
+//             list.splice(parseInt(indexNum),1);
+//             greeting = 1;
+//             toDoCLI();
+//         } else if (answer === "q") {
+//             console.log("See you soon!")
+//             greeting = 0;
+//             rl.close();
+//         } else {
+//             console.log("not a valid option")
+//             greeting = 1;
+//             toDoCLI();
+//         }
+//     })
+// }
+
+
+toDoCLI()
